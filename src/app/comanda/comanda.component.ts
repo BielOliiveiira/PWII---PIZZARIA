6fc73d6d8a29515d1f905c9404bdf68ecac446f1
BielOliiveiira <gabriel_vm@live.com>
2019-11-04 23:06:36 -0200

    Exercicio8 e 9

--- src/app/comanda/comanda.component.ts
+++ src/app/comanda/comanda.component.ts
@@ -18,7 +18,28 @@ export class ComandaComponent implements OnInit {
       pizza : this.formBuilder.control(''),
       recheio : this.formBuilder.control(''),
       pessoas : this.formBuilder.control(''),
+      taxaServico : this.formBuilder.control(false)
     })
   }
 
+  onProcessar(){
+    let vlChopp:number = this.formComanda.value.chopp * 7.30;
+    let vlPizza:number = this.formComanda.value.pizza * 31.50;
+    let vlRecheio:number = this.formComanda.value.recheio * 5.90;
+    let vlPessoas:number = this.formComanda.value.pessoas;
+    let taxaServico:boolean = this.formComanda.value.taxaServico;
+    let vlConsumo:number = (vlChopp + vlPizza + vlRecheio);
+    let vlTaxaServico: number = vlConsumo * 0.1;
+    let vlTotal:number;
+    let vlPorPessoa:number = vlTotal / vlPessoas;
+
+    if (taxaServico == true)
+    {
+      vlConsumo + vlTaxaServico == vlTotal
+    }
+    
+    
+    
+
+  }
 }
